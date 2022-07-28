b25319155375aea4e04b8c42d0e35b6adcd03a91
errengonzalez <errengonzalez@gmail.com>
2022-07-28 18:25:57 -0300

    PRIMER CLASE SUBIDA

---
+++ CLASE_1_TEST/src/CLASE_1_TEST.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : CLASE_1_TEST.c
+ Author      : rodrigo
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!HOLA!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
