1fb5772ff1239921c0c4eed639da932e8e4bbce2
errengonzalez <errengonzalez@gmail.com>
2022-08-31 14:13:49 -0300

    clase 3

---  CLASES_3_TEST/src/CLASES_3_TEST.c
+++  CLASES_3_TEST/src/CLASES_3_TEST.c
@@ -7,11 +7,45 @@
  Description : Hello World in C, Ansi-style
  ============================================================================
  */
+	// CLASE 3 FUNCIONES
+
 
 #include <stdio.h>
 #include <stdlib.h>
 
+int CalcularSuma(int numeroUno, int numeroDos);
+
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	setbuf(stdout, NULL);
+	int numeroUno;
+	int numeroDos;
+	int resultado;
+	/*
+	 * 			BENEFICIOS DE USAR FUNCIONES:
+	 *
+	 * 			-MAS FACIL DE LEER
+	 * 			-REUTILIZACION
+	 *
+	 * 			TIPO DATO - NOMNREFUNCION (PARAMETROUNO, PARAMETRODOS);
+	 *
+	 * */
+
+	printf("INGRESE UN NUMERO \n");
+	scanf("%d", &numeroUno);
+
+	printf("INGRESE UN NUMERO \n");
+	scanf("%d", &numeroDos);
+
+	resultado = CalcularSuma(numeroUno, numeroDos);
+
+	printf("El valor de la suma es: %d\n",resultado);
+
 	return EXIT_SUCCESS;
+}
+
+int CalcularSuma(int numeroUno, int numeroDos)
+{
+	int resultado;
+	resultado = numeroUno + numeroDos;
+	return resultado;
 }
--- CLASE_1_TEST/src/CLASE_1_TEST.c
+++ CLASE_1_TEST/src/CLASE_1_TEST.c
@@ -1,89 +1,28 @@
-/*
- ============================================================================
- Name        : CLASE_1_TEST.c
- Author      : rodrigo
- Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
- ============================================================================
- */
-
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-int main(void)
-{
+
+int main(void) {
 	setbuf(stdout, NULL);
-	int num1;
-	int num2;
-	int num3;
+		//primer programacion
+
+		// pedir dos numeros al usuario
+		// sumarlos
+		//mostrar suma
+
+	int primerNumero;
+	int segundoNumero;
 	int resultado;
 
-	// PUNTO 1 GUIA TEMA 1
+	printf("\n Ingrese primer numero \n");
+	scanf("%d", &primerNumero);
 
+	printf("\n Ingrese segundo numero \n");
+	scanf("%d", &segundoNumero);
 
-	printf("Ingrese su primer numero ");
-	scanf("%d",&num1); // -> AMPERSAN INDICA LA DIRECCION DE MEMORIA DONDE SE VA A ALMACENAR EL DATO
+	resultado = primerNumero + segundoNumero;
 
-	printf("Ingrese su segundo numero ");
-	scanf("%d",&num2);
+	printf(" Tu resultado es: %d", resultado);
 
-	resultado = num1 + num2;
-	printf("La suma entre %d y %d da como resultado: %d",num1, num2, resultado);
-
-
-	// 	PUNTO 2 GUIA TEMA 1
-
-	printf("Ingrese su primer numero ");
-	scanf("%d",&num1);
-
-	printf("Ingrese su segundo numero ");
-	scanf("%d",&num2);
-
-	printf("Ingrese su tercer numero ");
-	scanf("%d",&num3);
-
-	if(num1 > num2 && num1 > num3)
-	{
-		printf("El mayor de los numeros es el primero");
-	}
-	else
-	{
-		if(num2 > num3 && num2 > num1)
-		{
-			printf("El mayor de los numeros es el segundo");
-		}
-		else
-		{
-			printf("El mayor de los numeros es el tercero");
-		}
-	}
-
-
-	// PUNTO 3 GUIA TEMA 1
-
-	if(num1 != num2 && num1 != num3 && num2 != num3)
-	{
-		if((num1 > num2 && num1 < num3) || (num1 > num3 && num1 < num2))
-		{
-			printf(" %d %d %d el %d es del medio", num1, num2, num3, num1);
-		}
-		else
-		{
-			if((num2 >num1 && num2 < num3) || ( num2 < num1 && num2 > num3))
-			{
-				printf(" %d %d %d el %d es del medio", num1, num2, num3, num2);
-			}
-			else
-			{
-				printf(" %d %d %d el %d es del medio", num1, num2, num3, num3);
-			}
-		}
-	}
-	else
-	{
-		printf(" %d %d %d No hay numero del medio ",num1, num2, num3);
-	}
 	return EXIT_SUCCESS;
 }
-	//  %c (char) , %d (int) , %f (float)
